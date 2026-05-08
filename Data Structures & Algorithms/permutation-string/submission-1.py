54587f42f6d347d98daf88dc63ba5a8b8a11b5af
Sujan <56094667+Sujan30@users.noreply.github.com>
2026-05-08 11:51:40 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        
+        for r in range(len(s1), len(s2)+1):
+            print(s2[l:r])
+            if sorted(s2[l:r]) == sorted(s1):
+                return True
+            l+=1
+        return False
